262cf3bb35921c8221d042380992fa8d18339aea
zearaujo11 <ze.b.araujo@gmail.com>
2023-12-05 00:03:27 +0000

    new changes in redistributing sls when a new server is added

--- Server.py
+++ Server.py
@@ -9,6 +9,8 @@ import re
 
 LRU_READY = "\x01"
 SHOPPINGLIST = "sl:"
+RING = "ring:"
+REP = "sl_rep:"
 REPLICATION_FACTOR = 2
 
 class Server: 
@@ -93,6 +95,7 @@ class Server:
             return []
 
         replica_servers = []
+
         for i in range(1, min(REPLICATION_FACTOR + 1, num_servers)):
             index = (source_index + i) % num_servers
             next_server = server_ports[index]
@@ -111,10 +114,45 @@ class Server:
             server = self.context.socket(zmq.DEALER)
             server.connect("tcp://localhost:" + str(self.servers[str(successor)]))
             print("Sending to server: ", self.servers[str(successor)])
-            server.send(("sl:" + json.dumps(shoppinglist) + ":" + str(self.key)).encode('utf-8'))
+            server.send((REP + json.dumps(shoppinglist) + ":" + str(self.key)).encode('utf-8'))
 
         return 
+    
+    def find_lost_shopping_lists_for_server(self):
+        servers_copy = self.servers.copy()
+        del servers_copy['timestamp']
 
+        hash_range = [int(self.key), self.get_successors(self.key)[0]]
+
+        print("HASH RANGE: ", hash_range)
+
+        lost_shopping_lists = []
+
+        for shopping_list in self.shopping_lists: 
+            if not (hash_range[0] <= shopping_list["key"] < hash_range[1]): 
+                lost_shopping_lists.append(shopping_list)
+
+        return lost_shopping_lists, hash_range[1]
+    
+    def transfer_shopping_lists(self):
+        lost_shopping_lists, server_to_send = self.find_lost_shopping_lists_for_server()
+
+        server_to_send = self.servers[str(server_to_send)]
+
+        print("LOST SHOPPING LISTS: ", lost_shopping_lists)
+
+        print("BEFORE TRANSFER: ", self.shopping_lists)
+
+        for shopping_list in lost_shopping_lists:
+            sl = json.dumps(shopping_list)
+            server = self.context.socket(zmq.DEALER)
+            server.connect("tcp://localhost:" + str(server_to_send))
+            print("Sending to server: ", str(server_to_send))
+            server.send(("sl:" + sl).encode('utf-8'))
+            self.send_replicas(sl)
+            self.shopping_lists.remove(shopping_list)
+
+        print("AFTER TRANSFER: ", self.shopping_lists)
 
     def run(self):
 
@@ -131,7 +169,7 @@ class Server:
         poller.register(worker, zmq.POLLIN)
 
         while True:
-            start_time = time.time()
+            #start_time = time.time()
 
             events = dict(poller.poll(timeout=1000))
 
@@ -141,6 +179,16 @@ class Server:
                 if(SHOPPINGLIST in message_received[1].decode('utf-8')):
                     print("Received shopping list")
                     message = message_received[1].decode('utf-8')[3:]
+    
+                    self.shopping_lists.append(json.loads(message))
+
+                    print("Shopping lists: ", self.shopping_lists)
+                    #self.send_replicas(self.shoppinglist)
+
+                if REP in message_received[1].decode('utf-8'):
+                    message = message_received[1].decode('utf-8')[7:]
+
+                    print("MESSAGE: ", message)
                     
                     last_colon_index = message.rfind(":")
 
@@ -150,6 +198,8 @@ class Server:
                     if key_part.startswith(":"):
                         key_part = key_part[1:]
 
+                    print("KEY_PART: ", key_part)
+                    
                     shoppinglist = json.loads(json_part)
 
                     sls = self.replicas[key_part] if key_part in self.replicas else []
@@ -158,15 +208,19 @@ class Server:
 
                     print("Replicas: ", self.replicas)
 
-                if "ring:" in message_received[1].decode('utf-8'):
+                if RING in message_received[1].decode('utf-8'):
                     #print("Received ring")
 
                     message = json.loads(message_received[1].decode('utf-8')[5:])
 
-                    self.servers = message
+                    #self.servers = message
+                    #print(message)
 
                     if message["timestamp"] > self.servers["timestamp"]:
                         self.servers = message
+                        print("AAA")
+                        self.transfer_shopping_lists()
+                        print("BBB")
 
             if worker in events and events[worker] == zmq.POLLIN:
                 msg = worker.recv_multipart()
@@ -194,10 +248,10 @@ class Server:
                     print(msg)
                     worker.send_multipart(msg)      
             
-            elapsed_time = time.time() - start_time
+            """ elapsed_time = time.time() - start_time
 
             if elapsed_time < 1:
-                time.sleep(1 - elapsed_time)  # Sleep for the remaining seconds
+                time.sleep(1 - elapsed_time) """
 
             self.send_servers_ring()
             
