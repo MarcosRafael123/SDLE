c851152c262ed4aaa3f101566e6f80f746e5653f
Zé Araújo <72151399+zearaujo11@users.noreply.github.com>
2023-12-09 18:51:02 +0000

    commenting some lines

--- ShoppingListCRDT.py
+++ ShoppingListCRDT.py
@@ -50,7 +50,7 @@ class ShoppingListCRDT:
         print(self.get_items())
 
 
-local_shopping_list = ShoppingListCRDT({'apple': 2, 'banana': 1}, {'apple': 1})
+""" local_shopping_list = ShoppingListCRDT({'apple': 2, 'banana': 1}, {'apple': 1})
 remote_shopping_list = ShoppingListCRDT({'banana': 2, 'orange': 1}, {'banana': 1})
 
 local_shopping_list.add_item('grape')
@@ -60,4 +60,4 @@ remote_shopping_list.remove_item('orange')
 
 merged_shopping_list = local_shopping_list.merge(remote_shopping_list)
 
-print("Merged Shopping List Items:", merged_shopping_list.get_items())
+print("Merged Shopping List Items:", merged_shopping_list.get_items()) """
